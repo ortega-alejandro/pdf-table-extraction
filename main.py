d3ea2bfbf4fefa63057d4e63be82202591245cd5
brookeerickson <40547805+brookeerickson@users.noreply.github.com>
2018-07-12 12:29:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,11 +13,10 @@ def main():
     t2 = time.time()
     t = t2 - t1
     print(t)'''
-    my_pdf = "sample.pdf"
+    my_pdf = "desktop/OneDrive_2018-05-27/ml/3AC1DACD68A35562618B2A9D7B92DE841964B.pdf"
     ontology_path = "file:///users/brookeerickson/downloads/root-ontology-v9.owl"
-    ontologies.run(my_pdf, ontology_path)
-    # If you want to clean the directory with pictures, call pdfp.clean('images')
-
+    onto = get_ontology(ontology_path).load()
+    ontologies.run(my_pdf, ontology_path, page_threshold=0.005, threshold=0.03, number_of_levels=3, print_output=True, print_debug=False)
 
 if __name__ == "__main__":
     main()
