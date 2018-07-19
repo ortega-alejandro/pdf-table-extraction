fa0cea7a28341708a2d07f0624a689b008da5c97
Alejandro Ortega <ortegaalejandro55@gmail.com>
2018-07-19 11:28:27 -0400

    Open Knowledge

--- WebScrapeOpenKnowledge.py
+++ WebScrapeOpenKnowledge.py
@@ -22,21 +22,71 @@ xlsx_links = []
 URL_PREFIX = re.search('.*org/|.*com/|.*edu/', URL).group(0)
 prefix_length = len(URL_PREFIX)
 
-
-
 page_num = 1
-while all_links:
+
+
+def get_next_page():
     main_url = all_links[0]
-    print (main_url)
-    response = get(main_url)
-    html_soup = BeautifulSoup(response.text, 'lxml')
-    pages = html_soup.find_all('a', text=NEXT_TEXT)
-    print('Page length ', len(pages))
+    print(main_url)
+    responses = get(main_url)
+    html_soups = BeautifulSoup(responses.text, 'lxml')
+    page = html_soups.find_all('a', text=NEXT_TEXT)
     print('PAGE: '+str(page_num))
-    if len(pages)>0:
-        # pages = pages[0].find('a',href=True)
-        pages = URL_PREFIX+pages[0]['href']
-        # pages = URL_PREFIX+pages['href']
+    if len(page)>0:
+        page = URL_PREFIX+page[0]['href']
+    print(page)
+    return page
+
+
+def tags_by_date(TAGS, DATE_TAG, DATE_FORM, links, html_soup, pages):
+    for tag_type,tag in TAGS:
+        links_page = html_soup.find_all(tag_type, class_ = tag)
+        if len(links_page)==0:
+            continue
+        for each in links_page:
+                date = each.find(DATE_TAG[0],class_ = DATE_TAG[1])
+                print ("DATE")
+                print (date)
+                if date is not None:
+                    date = date.text.strip()
+                    date = datetime.datetime.strptime(date,DATE_FORM).date()
+                    last_date = datetime.datetime.strptime(LAST_SCRAPE_DATE,DATE_FORM).date()
+                    if date>=last_date:
+                        links.append(each)
+                    elif SORTED_SITE:
+                        pages=''
+                        break
+                else:
+                    links.append(each)
+    return links, pages
+
+
+def scrape_page(links, URL_PREFIX, pdf_links, xlsx_links, links_visited, all_links):
+    for each in links:
+        anchor_tags = each.find_all('a',href=True)
+        for link in anchor_tags:
+            pdf_url= link['href']
+            if pdf_url[:prefix_length] != URL_PREFIX:
+                pdf_url = URL_PREFIX+str(pdf_url)
+            if pdf_url not in links_visited:
+                links_visited.append(pdf_url)
+                if 'pdf' in pdf_url:
+                    pdf_links.append(pdf_url)
+                elif 'xlsx' in pdf_url:
+                    xlsx_links.append(pdf_url)
+                else:
+                    all_links.append(pdf_url)
+    if 'pdf' in current:
+        pdf_links.append(current)
+    elif 'xlsx' in current:
+        xlsx_links.append(current)
+    all_links.remove(current)
+    links_visited.append(current)
+    return pdf_links, xlsx_links, links_visited, all_links
+
+
+while all_links:
+    pages = get_next_page()
     while all_links:
         current = all_links[0]
         response = get(current)
@@ -66,7 +116,6 @@ while all_links:
 
                     last_date = datetime.datetime.strptime(LAST_SCRAPE_DATE, DATE_FORM2).date()
                     if date >= last_date:
-                        print('greater than')
                         links.append(each)
                     elif SORTED_SITE:
                         pages = ''
@@ -99,8 +148,6 @@ while all_links:
     if len(pages) > 0:
         all_links.append(pages)
         page_num += 1
-        if page_num == 3:
-            break
 
     
 print ('all_links')
