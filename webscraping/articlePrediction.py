f9dfb7ba79c3699ec74a4dd7b3a0a39f9084cdc2
jade457 <jade.wu86@gmail.com>
2018-07-30 12:02:44 -0400

    add training data

--- webscraping/articlePrediction.py
+++ webscraping/articlePrediction.py
@@ -16,7 +16,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
 import os
-import re
 import pickle
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
